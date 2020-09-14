712a61e1f9438fc7899385241c75507fb33adab8
Miyo Alpízar <miyo.naval@gmail.com>
2020-09-14 13:45:18 -0500

    Initial Commit

---
+++ RealTimeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeChat
+//
+//  Created by Miyo Alpízar on 14/09/20.
+//  Copyright © 2020 Miyo Alpízar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
